6c2a17b1b5956f084d36ca06189154965796d095
yogitoeolgu-star <yogitoeolgu@gmail.com>
2026-02-10 21:34:45 +0100

    Add AppConstants class to constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  static const String mangaDexApi = 'https://api.mangadex.org';
+  static const String manhwaTag = 'b9af3a63-f058-46de-a9a0-e0c13906197a';
+  static const Duration apiTimeout = Duration(seconds: 15);
+  static const int itemsPerPage = 20;
+}
