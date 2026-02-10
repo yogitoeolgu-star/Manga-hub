9d48eb745a0e37d705248029bcb88bc4e24d68bd
yogitoeolgu-star <yogitoeolgu@gmail.com>
2026-02-10 21:37:34 +0100

    Update ManhwaProvider implementation with ChangeNotifier for state management

--- lib/providers/manhwa_provider.dart
+++ lib/providers/manhwa_provider.dart
@@ -1,3 +1,26 @@
-class ManhwaProvider {
-    // Class implementation goes here
+import 'package:flutter/material.dart';
+
+class ManhwaProvider extends ChangeNotifier {
+  // Your state variables
+  bool _isLoading = false;
+  List<String> _manhwaList = [];
+
+  // Getters for state
+  bool get isLoading => _isLoading;
+  List<String> get manhwaList => _manhwaList;
+
+  // Fetch manhwa data
+  Future<void> fetchManhwa() async {
+    _isLoading = true;
+    notifyListeners();
+
+    // Simulate network request
+    await Future.delayed(Duration(seconds: 2));
+    _manhwaList = ['Manhwa 1', 'Manhwa 2', 'Manhwa 3'];
+
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  // Additional methods to manipulate state can be added below
 }
